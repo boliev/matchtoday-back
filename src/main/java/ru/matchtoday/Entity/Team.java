057321bf3bf56518db4ef328230767e37f3c3397
Vladimir Boliev <voff.web@gmail.com>
2019-08-31 08:55:48 +0300

    Team entity

---
+++ src/main/java/ru/matchtoday/Entity/Team.java
@@ -0,0 +1,36 @@
+package ru.matchtoday.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String country;
+
+    protected Team() {
+    }
+
+    public Team(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
